2569986fda4335f8d388b75a4f15d86c9defe891
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2024-12-30 13:12:38 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,14 @@
+
+provider "aws" {
+  region     = var.region
+ 
+}
+ 
+resource "aws_instance" "ins1" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+ 
+  tags = {
+    Name = "my-instnc"
+  }
+}
